0791c6f02b558666da5939e0c5f3ba5397c849f4
Sulaiman Yaacob <sulaimanyaacob99@gmail.com>
2021-03-17 12:35:44 +0800

    improved

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -1,19 +1,25 @@
 import React from "react";
 import "./ContentStyle.css";
+import Zoom from "react-reveal/Zoom";
+import Jello from "react-reveal/Jello";
 
 function Content() {
   return (
     <div className="content">
-      <h3>Introduction</h3>
-      <p>
-        I am a Web Developer and a student currently pursuing a degree. 2013 was
-        the year I started coding and became immersed in the field of coding. My
-        primary motivation was to make my own video game, but as time passed, I
-        became interested in web development. While I have some coding
-        experience, I would not consider myself a skilled programmer at this
-        time because I am still honing my skills to become one.
-      </p>
-      <hr></hr>
+      <Zoom>
+        <h3>Introduction</h3>
+        <p>
+          I am a Web Developer and a student currently pursuing a degree. 2013
+          was the year I started coding and became immersed in the field of
+          coding. My primary motivation was to make my own video game, but as
+          time passed, I became interested in web development. While I have some
+          coding experience, I would not consider myself a skilled programmer at
+          this time because I am still honing my skills to become one.
+        </p>
+      </Zoom>
+      <Jello>
+        <hr></hr>
+      </Jello>
     </div>
   );
 }
