069341e1b72301ea374642f4fb57277b090d004c
Sulaiman Yaacob <sulaimanyaacob99@gmail.com>
2021-03-24 13:23:42 +0800

    update

--- src/components/Content/Hobby.js
+++ src/components/Content/Hobby.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React, { useState, useEffect, Fragment } from "react";
+import React, { useState, useEffect } from "react";
 
 function Hobby() {
   const [posts, setPost] = useState([]);
