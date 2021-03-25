0e7a1f225d9d5fc9e6143533b36984bc246226f2
Sulaiman Yaacob <sulaimanyaacob99@gmail.com>
2021-03-25 15:56:33 +0800

    fix

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ function App() {
   return (
     <>
       <div className="App">
-        {/* <HeaderLink />
+        <HeaderLink />
         <Portfolio />
-        <Content /> */}
+        <Content />
       </div>
       {/* <Hobby /> */}
     </>
