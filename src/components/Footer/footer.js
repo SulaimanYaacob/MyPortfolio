b923e0d1b4c0ab81bf1698f3ca840a8cfed1bbe5
Sulaiman Yaacob <sulaimanyaacob99@gmail.com>
2021-05-16 13:41:51 +0800

    copy-right

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -8,7 +8,7 @@ function footer() {
         <p className="rest">Note that this is just a demo</p>
       </section>
       <hr></hr>
-      <div className="copyright">&#169; Sulaiman Yaacob</div>
+      <div className="copyright">&#169;2021 Sulaiman Yaacob</div>
     </div>
   );
 }
